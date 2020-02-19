7d83f908823f9cf14f5203ef9f6e3cba6cf20e17
Luca Gioppo <gioppoluca@libero.it>
2020-02-19 09:16:42 +0100

    adding monitoring

--- app/src/boot/mermaid.js
+++ app/src/boot/mermaid.js
@@ -16,3 +16,13 @@ Icon.Default.mergeOptions({
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
+
+import Countly from 'countly-sdk-web';
+import VueCountly from 'vue-countly';
+
+Vue.use(VueCountly, Countly, {
+  app_key: 'ef7879ac3b3a064c106d5fa27ba2f70603b8c2da',
+  url: 'https://monitoring.micadoproject.eu',
+});
+
+console.log(Vue.Countly === Countly);
