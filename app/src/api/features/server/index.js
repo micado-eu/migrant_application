dcecf4e68ceeab0a981de6f6004e08598fc4d747
Luca Gioppo <gioppoluca@libero.it>
2020-02-21 20:25:53 +0100

    fix untrusted certificates

--- app/src/api/features/server/index.js
+++ app/src/api/features/server/index.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
+import https from 'https';
 
 export default {
   fetchFeatures () {
     console.log("sono il client di features")
+    const agent = new https.Agent({
+        rejectUnauthorized: false
+      });
     return axios
-      .get('https://api.micadoproject.eu/feature/v01')
+      .get('https://api.micadoproject.eu/feature/v01', { httpsAgent: agent })
       .then(
         response => response.data
       ).catch(function (err) {
