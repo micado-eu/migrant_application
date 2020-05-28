31dd1c387e91dcd570c7419bd6d197db3bf2f578
Luca Gioppo <gioppoluca@libero.it>
2020-05-29 01:17:24 +0200

    Changing the chatbot

--- app/src/api/features/server/index.js
+++ app/src/api/features/server/index.js
@@ -5,16 +5,18 @@ export default {
   fetchFeatures () {
     console.log("sono il client di features")
     const agent = new https.Agent({
-        rejectUnauthorized: false
-      });
+      rejectUnauthorized: false
+    });
 
 
     return axios
-      .get('https://api.micadoproject.eu/db/v01/migrant_app_config', { httpsAgent: agent ,
-                                                         headers: {
-                                                          Authorization: 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlpqUm1ZVE13TlRKak9XVTVNbUl6TWpnek5ESTNZMkl5TW1JeVkyRXpNamRoWmpWaU1qYzBaZz09In0.eyJhdWQiOiJodHRwOlwvXC9vcmcud3NvMi5hcGltZ3RcL2dhdGV3YXkiLCJzdWIiOiJhZG1pbkBjYXJib24uc3VwZXIiLCJhcHBsaWNhdGlvbiI6eyJvd25lciI6ImFkbWluIiwidGllciI6IlVubGltaXRlZCIsIm5hbWUiOiJNaWdyYW50QXBwbGljYXRpb24iLCJpZCI6MSwidXVpZCI6bnVsbH0sInNjb3BlIjoiYW1fYXBwbGljYXRpb25fc2NvcGUgZGVmYXVsdCIsImlzcyI6Imh0dHBzOlwvXC9nYXRld2F5Lm1pY2Fkb3Byb2plY3QuZXU6NDQzXC9vYXV0aDJcL3Rva2VuIiwidGllckluZm8iOnsiQnJvbnplIjp7InN0b3BPblF1b3RhUmVhY2giOnRydWUsInNwaWtlQXJyZXN0TGltaXQiOjAsInNwaWtlQXJyZXN0VW5pdCI6bnVsbH19LCJrZXl0eXBlIjoiUFJPRFVDVElPTiIsInN1YnNjcmliZWRBUElzIjpbeyJzdWJzY3JpYmVyVGVuYW50RG9tYWluIjoiY2FyYm9uLnN1cGVyIiwibmFtZSI6IlBvc3RnUkVTVEFQSSIsImNvbnRleHQiOiJcL2RiXC92MDEiLCJwdWJsaXNoZXIiOiJhZG1pbiIsInZlcnNpb24iOiJ2MDEiLCJzdWJzY3JpcHRpb25UaWVyIjoiQnJvbnplIn1dLCJjb25zdW1lcktleSI6InZabFNMbTFnYzVOMzIxbnRIN2Ztd2tONTNvVWEiLCJleHAiOjM3MzAyOTc0MzgsImlhdCI6MTU4MjgxMzc5MSwianRpIjoiYmYyMjBiYjMtNjY3MC00OTA5LWI4OTctOTY2ZDVhZDdhN2M0In0.GORnM7Hfflrv8iNFbBOZoLg7475tnLaXwY2f88My_qVCAupJxqPihM901E5GNQUsL2I7PW9_ymbCPJki0FuaIhdXk4ovso11ghjWDkVH9fUoMm_FElNynOlWp7gPDwtXbS5sNI2nZHflvUmc9IYG70XJG6tWhg4hI8bW0sNr03gkQOjQzbUqSqHb__J_oLJye1IGi657oJUtXnVLSDfRHOKl7w8SATrSiR_K57SkT4xGmpLqYGmbXsoWFJT-FHe1-WVrGBvwk2kqZfjgjDUoUedrDR0F9T_YrVIuPtruGqR4gJGWBuXPciOSHYsGu12Oxg3zC1FwoptN0NA2AZ-oTg' //the token is a variable which holds the token
-                                                         }
-                                                       })
+      .get('https://api.micadoproject.eu/db/v01/migrant_app_config', {
+        httpsAgent: agent,
+        headers: {
+          //                                                          Authorization: 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlpqUm1ZVE13TlRKak9XVTVNbUl6TWpnek5ESTNZMkl5TW1JeVkyRXpNamRoWmpWaU1qYzBaZz09In0.eyJhdWQiOiJodHRwOlwvXC9vcmcud3NvMi5hcGltZ3RcL2dhdGV3YXkiLCJzdWIiOiJhZG1pbkBjYXJib24uc3VwZXIiLCJhcHBsaWNhdGlvbiI6eyJvd25lciI6ImFkbWluIiwidGllciI6IlVubGltaXRlZCIsIm5hbWUiOiJNaWdyYW50QXBwbGljYXRpb24iLCJpZCI6MSwidXVpZCI6bnVsbH0sInNjb3BlIjoiYW1fYXBwbGljYXRpb25fc2NvcGUgZGVmYXVsdCIsImlzcyI6Imh0dHBzOlwvXC9nYXRld2F5Lm1pY2Fkb3Byb2plY3QuZXU6NDQzXC9vYXV0aDJcL3Rva2VuIiwidGllckluZm8iOnsiQnJvbnplIjp7InN0b3BPblF1b3RhUmVhY2giOnRydWUsInNwaWtlQXJyZXN0TGltaXQiOjAsInNwaWtlQXJyZXN0VW5pdCI6bnVsbH19LCJrZXl0eXBlIjoiUFJPRFVDVElPTiIsInN1YnNjcmliZWRBUElzIjpbeyJzdWJzY3JpYmVyVGVuYW50RG9tYWluIjoiY2FyYm9uLnN1cGVyIiwibmFtZSI6IlBvc3RnUkVTVEFQSSIsImNvbnRleHQiOiJcL2RiXC92MDEiLCJwdWJsaXNoZXIiOiJhZG1pbiIsInZlcnNpb24iOiJ2MDEiLCJzdWJzY3JpcHRpb25UaWVyIjoiQnJvbnplIn1dLCJjb25zdW1lcktleSI6InZabFNMbTFnYzVOMzIxbnRIN2Ztd2tONTNvVWEiLCJleHAiOjM3MzAyOTc0MzgsImlhdCI6MTU4MjgxMzc5MSwianRpIjoiYmYyMjBiYjMtNjY3MC00OTA5LWI4OTctOTY2ZDVhZDdhN2M0In0.GORnM7Hfflrv8iNFbBOZoLg7475tnLaXwY2f88My_qVCAupJxqPihM901E5GNQUsL2I7PW9_ymbCPJki0FuaIhdXk4ovso11ghjWDkVH9fUoMm_FElNynOlWp7gPDwtXbS5sNI2nZHflvUmc9IYG70XJG6tWhg4hI8bW0sNr03gkQOjQzbUqSqHb__J_oLJye1IGi657oJUtXnVLSDfRHOKl7w8SATrSiR_K57SkT4xGmpLqYGmbXsoWFJT-FHe1-WVrGBvwk2kqZfjgjDUoUedrDR0F9T_YrVIuPtruGqR4gJGWBuXPciOSHYsGu12Oxg3zC1FwoptN0NA2AZ-oTg' //the token is a variable which holds the token
+          "apikey": "eyJ4NXQiOiJaalJtWVRNd05USmpPV1U1TW1Jek1qZ3pOREkzWTJJeU1tSXlZMkV6TWpkaFpqVmlNamMwWmc9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImFwcGxpY2F0aW9uIjp7Im93bmVyIjoiYWRtaW4iLCJ0aWVyIjoiVW5saW1pdGVkIiwibmFtZSI6IkRlZmF1bHRBcHBsaWNhdGlvbiIsImlkIjoxLCJ1dWlkIjoiNzBmNGQ5MzEtNmQxYi00ZGJiLThiZGItNjgyNGViZWRhNjA5In0sInRpZXJJbmZvIjp7IlVubGltaXRlZCI6eyJzdG9wT25RdW90YVJlYWNoIjp0cnVlLCJzcGlrZUFycmVzdExpbWl0IjowLCJzcGlrZUFycmVzdFVuaXQiOm51bGx9fSwia2V5dHlwZSI6IlBST0RVQ1RJT04iLCJzdWJzY3JpYmVkQVBJcyI6W3sic3Vic2NyaWJlclRlbmFudERvbWFpbiI6ImNhcmJvbi5zdXBlciIsIm5hbWUiOiJQb3N0Z1JFU1RBUEkiLCJjb250ZXh0IjoiXC9kYlwvdjAxIiwicHVibGlzaGVyIjoiYWRtaW4iLCJ2ZXJzaW9uIjoidjAxIiwic3Vic2NyaXB0aW9uVGllciI6IlVubGltaXRlZCJ9XSwiaWF0IjoxNTkwNjUwMDU3LCJqdGkiOiJmZTJlNjdlNi1hZjczLTQzYjItYjVjMS1kNWQxMmIxNjhmZWQifQ==.WKZBZE6aM_R2Ps0_g9eJOt1aKF5zhTMQ4wpD4cO35AX0tZL0-VA8uccZZJW770aouUDCtZs0840E0mykZOpNdFoRO0QeIQkVshmh43QdspPBVjKB7BKfy49LxgZi9SeB-uLxqZwPz9Fxd-d5BY7I8Q1Y7IqyULugeZeSPRt3YUul1dvFzwTJTQ1sD1GphBBdO03YRC4bOyPbtySy55tCBV2LzqTHQ45pUf0boCidIlB7OO5jYkwiIYtjUxASIxRGduUcxJ4ghfhWtUBuEtpIykMh6BZ88TbRKE3_UqZ-veel6sCiOjDgTgW7J3M2l5Az0ivDisEzaf8qtCTQO7u4DA=="
+        }
+      })
       .then(
         response => {
           console.log(response.data)
--- app/src/pages/Chatbot.vue
+++ app/src/pages/Chatbot.vue
@@ -1,21 +1,43 @@
 <template>
   <div class="q-pa-md row justify-center">
     <div style="width: 100%; max-width: 400px">
-      <q-chat-message  v-for="message in messages" :key="message.id" :text="[message.text]"
+      <q-chat-message
+        v-for="message in messages"
+        :key="message.id"
+        :text="[message.text]"
         :name="message.user"
         :stamp="message.timestamp"
         bg-color="message.color"
         text-color="black"
         :sent="message.sent"
-      />
+      >
+        <q-btn
+          v-if="message.btn"
+          :label="message.btn_label"
+          :to="message.btn_link"
+        />
+      </q-chat-message>
     </div>
     <form>
       <div class="form-group">
         <label for="exampleInputEmail1">Hi I'm your helping chatbot ... ask me something</label>
-        <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter question" v-model="word" >
-        <small id="emailHelp" class="form-text text-muted">I'll try to answer to my best knowledge</small>
+        <input
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter question"
+          v-model="word"
+        >
+        <small
+          id="emailHelp"
+          class="form-text text-muted"
+        >I'll try to answer to my best knowledge</small>
       </div>
-      <button type="submit" @click="getWord" class="btn btn-primary">Submit</button>
+      <button
+        type="submit"
+        @click="getWord"
+        class="btn btn-primary"
+      >Submit</button>
     </form>
   </div>
 </template>
@@ -23,20 +45,23 @@
 <script>
 import ChatService from '../services/ChatService'
 
+// TODO for generating phrases better
+// https://github.com/sindrekjr/sentence-engine
+
 export default {
   name: 'Chatbot',
   props: {
     msg: String
   },
   data () {
-      return {
-          word: 'test',
-          wordData: '',
-          n_messages: 2
-      }
+    return {
+      word: 'test',
+      wordData: '',
+      n_messages: 2
+    }
   },
   created () {
-//    this.loading = true
+    //    this.loading = true
     console.log(this.$store);
     this.$store.dispatch('chatbot/fetchMessages')
       .then(messages => {
@@ -44,32 +69,31 @@ export default {
       })
   },
   computed: {
-    messages() {
+    messages () {
       return this.$store.state.chatbot.messages
     }
   },
   methods: {
-      async getWord () {
-        console.log("before calling")
-        this.n_messages++
-        this.$store.commit('chatbot/addMessage',{"id":this.n_messages, "user":"Luca", "text":this.word, "timestamp":"13:55", "sent":true, "color": "#cccccc"})
-        const response = await ChatService.getWord({ word: this.word })
-        let responses = response.data
-        console.log("after calling")
-        console.log(response)
-        console.log(responses)
-        this.wordData = responses[0]
-        let today = new Date();
-        let time = today.getHours() + ":" + today.getMinutes()
-        this.n_messages++
+    async getWord () {
+      console.log("before calling")
+      this.n_messages++
+      this.$store.commit('chatbot/addMessage', { "id": this.n_messages, "user": "Luca", "text": this.word, "timestamp": "13:55", "sent": true, "color": "#cccccc", btn: false })
+      const response = await ChatService.getWord({ word: this.word })
+      let responses = response.data
+      console.log("after calling")
+      console.log(response)
+      console.log(responses)
+      this.wordData = responses[0]
+      let today = new Date();
+      let time = today.getHours() + ":" + today.getMinutes()
+      this.n_messages++
 
-        this.$store.commit('chatbot/addMessage',{"id":this.n_messages, "user":"Micado", "text":responses[0].text, "timestamp":time, "sent":false, "color": "#cccccc"})
-      }
+      this.$store.commit('chatbot/addMessage', { "id": this.n_messages, "user": "Micado", "text": responses[0].text, "timestamp": time, "sent": false, "color": "#cccccc", btn: true, btn_label: "test", btn_link: "/processes" })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
