b54ae8fd119901d6331e49a6602eeaeb538e1ca8
Iván Chicano <ivchicano@gmail.com>
2020-06-03 14:25:34 +0200

    Refactor GlossaryEditorViewer to search for ids instead of titles when showing tooltip

--- app/src/store/glossary/getters.js
+++ app/src/store/glossary/getters.js
@@ -1,10 +1,10 @@
-export function glossary (state) {
+export function glossary(state) {
     return state.glossary
 }
 
-export function glossaryElemByTitle(state) {
-    let getterFunc = function(title) {
-        return state.glossary.filter(g => g.title === title)[0]
+export function glossaryElemById(state) {
+    let getterFunc = function (id) {
+        return state.glossary.filter(g => g.id == id)[0]
     }
     return getterFunc
 }
