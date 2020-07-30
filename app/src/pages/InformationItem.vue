aa28eb508383148d82c45c846e04faf800605db0
Iván Chicano <ivchicano@gmail.com>
2020-07-30 13:09:34 +0200

    Remove dead code

--- app/src/pages/InformationItem.vue
+++ app/src/pages/InformationItem.vue
@@ -1,20 +1,8 @@
 <template>
   <div class="q-pa-md">
     <span v-if="loading">Loading...</span>
-    <div
-      class="item q-px-md"
-      v-if="!loading"
-    >
-      <h3 class="information-title">{{item.title}}</h3>
-      <span v-if="item.tags">Tags:</span>
-      <q-btn
-        v-for="tag in item.tags"
-        :key=tag
-        :label=tag
-        no-caps
-        rounded
-        class="q-ma-sm tag_btn"
-      />
+    <div class="item q-px-md" v-if="!loading">
+      <h3 class="information-title">{{ item.title }}</h3>
       <glossary-editor-viewer
         :content="item.description"
         class="q-mt-md description"
@@ -35,8 +23,8 @@
 </template>
 
 <script>
-import GlossaryEditorViewer from "../components/GlossaryEditorViewer"
-import { mapGetters, mapActions } from "vuex"
+import GlossaryEditorViewer from "../components/GlossaryEditorViewer";
+import { mapGetters, mapActions } from "vuex";
 export default {
   components: {
     "glossary-editor-viewer": GlossaryEditorViewer
@@ -46,32 +34,32 @@ export default {
       loading: true,
       id: -1,
       item: {},
-      lang: "",
-    }
+      lang: ""
+    };
   },
   methods: {
     ...mapActions("information", ["fetchInformation"]),
     goBack() {
-      this.$router.go(-1)
+      this.$router.go(-1);
     }
   },
   computed: {
-    ...mapGetters("information", ["informationElemById"]),
+    ...mapGetters("information", ["informationElemById"])
   },
   created() {
-    this.loading = true
-    this.lang = this.$userLang
-    this.id = this.$route.params.id
+    this.loading = true;
+    this.lang = this.$userLang;
+    this.id = this.$route.params.id;
     this.fetchInformation().then(() => {
-      let itemById = this.informationElemById(this.id)
-      let al = this.$userLang
-      let idx = itemById.translations.findIndex(t => t.lang === al)
-      let translated = Object.assign({}, itemById.translations[idx])
-      this.item = translated
-      this.loading = false
-    })
+      let itemById = this.informationElemById(this.id);
+      let al = this.$userLang;
+      let idx = itemById.translations.findIndex(t => t.lang === al);
+      let translated = Object.assign({}, itemById.translations[idx]);
+      this.item = translated;
+      this.loading = false;
+    });
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -82,9 +70,5 @@ $btn_secondary: #cdd0d2;
 .item {
   border: 1px solid $primary;
   border-radius: 10px;
-}
-.tag_btn {
-  background-color: $btn_secondary;
-  color: black;
 }
 </style>
