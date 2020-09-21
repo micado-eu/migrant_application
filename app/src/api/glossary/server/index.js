7f2883ed951b5914921643ced44a22ab907ac8d0
Iván Chicano <ivchicano@gmail.com>
2020-09-21 11:51:42 +0200

    Glossary now sees all content
    While we change the current published column to the translations table,migrants can  see all content. This will change in the future

--- app/src/api/glossary/server/index.js
+++ app/src/api/glossary/server/index.js
@@ -4,7 +4,7 @@ import { error_handler } from '../../../helper/utility'
 export default {
   fetchGlossary() {
     return axiosInstance
-      .get('/backend/1.0.0/glossaries/published?filter[include][0][relation]=translations', {
+      .get('/backend/1.0.0/glossaries?filter[include][0][relation]=translations', {
       })
       .then((response) => {
         return response.data
