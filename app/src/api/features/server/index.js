84aa0d6853d7b67babeb1eb90ce98b8bb77342e4
Luca Gioppo <gioppoluca@libero.it>
2020-10-30 10:33:47 +0100

    fixed the api call for featuresflag

--- app/src/api/features/server/index.js
+++ app/src/api/features/server/index.js
@@ -5,7 +5,7 @@ export default {
   fetchFeaturesFlags () {
     console.log("sono il client di features")
     return axiosInstance
-      .get('/backend/1.0.0/migrant-app-configs')
+      .get('/backend/1.0.0/active-features')
       .then(response => {
         console.log(response.data)
         return response.data
