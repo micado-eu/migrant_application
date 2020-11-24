12c824a048efb79a34f8bf2dc3ff45e240edd8bf
Iván Chicano <ivchicano@gmail.com>
2020-11-24 14:34:32 +0100

    Add spanish translation

--- app/src/i18n/index.js
+++ app/src/i18n/index.js
@@ -2,11 +2,13 @@ import enUS from './en-us'
 import it from './it'
 import ar from './ar'
 import de from './de'
+import es from './es'
 
 export default {
   'en-us': enUS,
   it: it,
   ar: ar,
-  de: de
+  de: de,
+  es: es
 
 }
