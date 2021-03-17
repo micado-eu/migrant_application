e4fdb0feaf04520fa9016a914a606c35a291f412
Luca Gioppo <gioppoluca@libero.it>
2021-03-17 17:28:45 +0100

    privacy policy page

---
+++ app/src/pages/Privacy.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    {{ $t( "privacy") }}
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      logged: false
+    }
+  },
+  created () {
+   
+  }
+}
+</script>
--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -49,6 +49,7 @@ const routes = [
       { path: '/settings', name:'settings', component: () => import('pages/ProfileSettings.vue'), meta: { auth: true } },
       { path: '/certificates', component: () => import('pages/Certificates.vue'), props: true, name: 'certificates' },
       { path: '/map', component: () => import('components/Map.vue')},
+      { path: '/privacy', component: () => import('pages/Privacy.vue')},
       { path: '/glossary/:id?', component: () => import('pages/Glossary.vue')  },
       { path: '/documents/edit/:thedocid', component: () => import('components/AddDocument.vue'), props: (route) => ({ ...route.params }), name: 'editdocument', meta: { auth: true } },
       { path: '/decision',  component: () => import('pages/DecisionTreePage.vue')},
