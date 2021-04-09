44954c5a3adc8f412ee6d3c27b4281f8deee6437
Iván Chicano <ivchicano@gmail.com>
2021-04-09 14:46:36 +0200

    Add talking labels to language selector

--- app/src/components/LanguageSelector.vue
+++ app/src/components/LanguageSelector.vue
@@ -8,55 +8,67 @@
       @click="layout = true"
     />-->
 
-    <q-dialog v-model="layout">
-      <q-layout
-        view="Lhh lpR fff"
-        container
-        class="bg-white"
-      >
-        <q-header>
-          <q-toolbar class="bg-white">
-            <q-space />
-            <q-btn
-              color="red"
-              flat
-              v-close-popup
-              round
-              dense
-              icon="close"
-            />
-          </q-toolbar>
-        </q-header>
+  <q-dialog v-model="layout">
+    <q-layout
+      view="Lhh lpR fff"
+      container
+      class="bg-white"
+    >
+      <q-header>
+        <q-toolbar class="bg-white">
+          <q-space />
+          <q-btn
+            color="red"
+            flat
+            v-close-popup
+            round
+            dense
+            icon="close"
+          />
+        </q-toolbar>
+      </q-header>
 
-        <q-page-container class="q-ma-sm">
-          <span v-if="loading">Loading...</span>
-          <q-page
-            v-else
-            padding
-            class="language_modal flex"
+      <q-page-container class="q-ma-sm">
+        <span v-if="loading">Loading...</span>
+        <q-page
+          v-else
+          padding
+          class="language_modal flex"
+        >
+          <q-toggle
+            class="q-mr-sm q-my-sm language_btn"
+            color="accent"
+            v-for="language in activeLanguages"
+            :key="language.isoCode"
+            @input="getValue(language)"
+            :id="language.isoCode"
+            :value="getToggleValue(language)"
+            :data-cy="'language_button_' + language.isoCode"
           >
-            <q-toggle
-              class="q-mr-sm q-my-sm language_btn"
-              color="accent"
-              v-for="language in activeLanguages"
-              :key="language.isoCode"
-              :label="language.name"
-              @input="getValue(language)"
-              :id="language.isoCode"
-              :value="getToggleValue(language)"
-              :data-cy="'language_button_' + language.isoCode"
-            />
-          </q-page>
-        </q-page-container>
-      </q-layout>
-    </q-dialog>
-  </div>
+            <div class="row">
+              <talking-label
+                :text="language.name"
+                class="col-shrink q-mr-sm"
+              ></talking-label>
+              <span class="col-auto">{{language.name}}</span>
+            </div>
+          </q-toggle>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </q-dialog>
+  <!-- </div> -->
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex"
 import { setLocale } from 'boot/i18n'
+import TalkingLabel from "./TalkingLabel.vue"
+
 export default {
+  components: {
+    TalkingLabel
+  },
   data() {
     return {
       loading: true,
@@ -68,7 +80,7 @@ export default {
     ...mapGetters('language', ['activeLanguages']),
   },
   methods: {
-    open(){
+    open() {
       this.layout = true
     },
     getValue(language) {
@@ -77,21 +89,21 @@ export default {
       this.$router.go()
     },
     getToggleValue(language) {
-      return this._selectedLanguage===language.lang
+      return this._selectedLanguage === language.lang
     },
     ...mapActions("language", ["fetchActiveLanguages"])
   },
   created() {
     this.fetchActiveLanguages().then(() => {
       this._selectedLanguage = this.$userLang
-      this.loading= false
+      this.loading = false
     })
   }
 }
 </script>
 <style scoped lang="scss">
-  .language_modal {
-    flex-direction: column;
-    flex-wrap: nowrap;
-  }
+.language_modal {
+  flex-direction: column;
+  flex-wrap: nowrap;
+}
 </style>
