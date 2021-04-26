b7d51fcd202f461973f17212bf6c159ffca75ac4
giuseppecsi <giuseppe.dorio@csi.it>
2021-04-26 16:58:56 +0200

    added pa logo, micado logo and eu logo on privacy policy page

--- app/src/pages/Privacy.vue
+++ app/src/pages/Privacy.vue
@@ -1,6 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-    {{ $t( "privacy") }}
+  <q-page class="flex flex-center column">
+    <div>
+      {{$t('privacy')}}
+    </div>
+  <div style="background-color:#EFEFEF; height:5px">
+    &nbsp;
+  </div>
+    <div class="row justify-center full-height full-width text-center">
+      <img
+        alt="Micado logo"
+        :src="this.$paLogo"
+        class="width"
+      >
+
+    </div>
+
+    <div class="row justify-center full-height full-width text-center">
+      <img
+        alt="Powered by Micado"
+        src="~assets/powered_Micado.png"
+      >
+      </div>
+      <div class="pad" text-caption>This project has received funding from the European Union’s H2020 Innovation Action under Grant Agreement No 822717.</div>
+      <img
+        alt="Funded by EU"
+        src="~assets/Flag_of_Europe.png"
+      >
+    
   </q-page>
 </template>
 
@@ -17,3 +43,9 @@ export default {
   }
 }
 </script>
+<style scoped>
+.pad{
+  padding-left:10px;
+  padding-right:10px
+}
+</style>
