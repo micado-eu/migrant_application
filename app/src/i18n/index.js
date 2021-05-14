7759856c1bd774c2ff753e5477b041f05a92c0b8
Iván Chicano <ivchicano@gmail.com>
2021-05-14 15:47:10 +0200

    Add chinese translation

--- app/src/i18n/index.js
+++ app/src/i18n/index.js
@@ -3,12 +3,14 @@ import it from './it'
 import ar from './ar'
 import de from './de'
 import es from './es'
+import zh_Hans from './zh_Hans'
 
 export default {
   'en-us': enUS,
   it: it,
   ar: ar,
   de: de,
-  es: es
+  es: es,
+  zh_Hans
 
 }
