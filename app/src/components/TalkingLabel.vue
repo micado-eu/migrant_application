744e1dbee1dbcb345d627916a22b47eb3891d037
giuseppecsi <giuseppe.dorio@csi.it>
2021-09-23 15:47:21 +0200

    when clicked, the text to speech button turns into a pause button

--- app/src/components/TalkingLabel.vue
+++ app/src/components/TalkingLabel.vue
@@ -1,27 +1,57 @@
 <template>
-<div :class="row" :style="this.container_style">
-  <div :class="title_col" :style="this.showing">
-    <q-icon style="margin-bottom:5px" :size="icon_size" v-if="this.icon" :name="this.icon" />
-     {{ this.Title }} 
+  <div :class="row" :style="this.container_style">
+    <div :class="title_col" :style="this.showing">
+      <q-icon
+        style="margin-bottom: 5px"
+        :size="icon_size"
+        v-if="this.icon"
+        :name="this.icon"
+      />
+      {{ this.Title }}
+    </div>
+
+    <div :class="icon_col" :style="icon_style">
+      <q-icon
+        v-if="!amISpeaking"
+        @click.stop="readClick(text)"
+        :id="this.text"
+        name="img:statics/icons/Icon - Auto Read  (selected).svg"
+        size="xsm"
+      />
+      <q-icon
+        v-else
+        @click.stop="readClick(text)"
+        :id="this.text"
+        name="img:statics/icons/pause.png"
+        size="xsm"
+      />
+    </div>
   </div>
-      
-  
-  <div :class="icon_col" :style="icon_style" >
-    <q-icon @click.stop="readClick(text)" :id="this.text" name="img:statics/icons/Icon - Auto Read  (selected).svg" size="xsm"  />
-  </div>
-</div>
 </template>
 
 <script>
 export default {
   // name: 'ComponentName',
-  data () {
+  data() {
     return {
-    }
+      amISpeaking: false,
+    };
   },
-  props: ["Title", "text", 'row', 'title_col', 'icon_col', 'icon_style', 'icon_class', 'icon', 'icon_size', 'showing', 'container_style'],
+  props: [
+    "Title",
+    "text",
+    "row",
+    "title_col",
+    "icon_col",
+    "icon_style",
+    "icon_class",
+    "icon",
+    "icon_size",
+    "showing",
+    "container_style",
+  ],
   methods: {
-  /* readClick(value) {
+    /* readClick(value) {
      var voice= ""
      switch(this.$userLang) {
   case 'nl':
@@ -50,26 +80,27 @@ export default {
         responsiveVoice.speak(value, voice)
      }
   },*/
-  readClick(value){
-    console.log("IN NEW TEXT TO SPEECH API")
-    var synth = window.speechSynthesis;
-    var amISpeaking = synth.speaking;
-    if(amISpeaking){
-      console.log("already speaking")
-      synth.cancel();
-    }
-    else{
-      //speechSynthesis.speak(new SpeechSynthesisUtterance(value));
-      console.log(synth.getVoices())
-      var u = new SpeechSynthesisUtterance();
-      u.text = value;
-      u.lang = this.$userLang;
-      u.rate = 1.0;
-      speechSynthesis.speak(u);
-    }
-
-
-  }
-  }
-}
+    readClick(value) {
+      console.log("IN NEW TEXT TO SPEECH API");
+      var synth = window.speechSynthesis;
+      this.amISpeaking = synth.speaking;
+      if (this.amISpeaking) {
+        console.log("already speaking");
+        synth.cancel();
+      } else {
+        //speechSynthesis.speak(new SpeechSynthesisUtterance(value));
+        console.log(synth.getVoices());
+        var u = new SpeechSynthesisUtterance();
+        u.text = value;
+        u.lang = this.$userLang;
+        u.rate = 1.0;
+        speechSynthesis.speak(u);
+        this.amISpeaking = true;
+        u.onend = (event) => {
+          this.amISpeaking = false;
+        };
+      }
+    },
+  },
+};
 </script>
