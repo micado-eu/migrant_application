cda9443f5f262552f26ec43cbe88befee90568ed
D'ORIO Giuseppe 2098 <giuseppe.dorio@csi.it>
2021-11-23 14:25:20 +0100

    retrying git workflow

--- app/src/pages/About.vue
+++ app/src/pages/About.vue
@@ -272,10 +272,10 @@ export default {
         answer: JSON.stringify(answer),
         answerDate: new Date().toISOString()
       }
-      console.log(formatted_results)
+      //console.log(formatted_results)
       this.saveSurveyAnswer(formatted_results)
       console.log("I am saving the results of the survey!!!!!")
-      this.setSurveyJSON(null)
+      this.setSurveyJSON(null) 
     },
   },
   created () {
