82aec677b81e6d38edf09f15b7b355c42e3d46e8
D'ORIO Giuseppe 2098 <giuseppe.dorio@csi.it>
2022-03-04 09:26:57 +0100

    trying fix for global css

--- app/src/App.vue
+++ app/src/App.vue
@@ -6,7 +6,16 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created(){
+    var customcss = document.createElement("link");
+    customcss.type = "text/css";
+    customcss.href = "/customcss/migrant.css"
+    customcss.rel = "stylesheet"
+    console.log("I'm custom css")
+    console.log(customcss)
+    document.getElementsByTagName('head')[0].appendChild(customcss);
+  }
 }
 </script>
  
