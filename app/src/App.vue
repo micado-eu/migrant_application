0f9610ec1dc7b1dedd608b50b80b1ade019b3ed3
D'ORIO Giuseppe 2098 <giuseppe.dorio@csi.it>
2022-03-04 10:35:34 +0100

    css

--- app/src/App.vue
+++ app/src/App.vue
@@ -17,13 +17,7 @@ export default {
     document.getElementsByTagName('head')[0].appendChild(customcss);
   },
   created(){
-    /*var customcss = document.createElement("link");
-    customcss.type = "text/css";
-    customcss.href = "/customcss/migrant.css"
-    customcss.rel = "stylesheet"
-    console.log("I'm custom css")
-    console.log(customcss)
-    document.getElementsByTagName('head')[0].appendChild(customcss);*/
+    
   }
 }
 </script>
