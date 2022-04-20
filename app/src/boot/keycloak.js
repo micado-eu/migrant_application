8b50abd8ca386eae87270563aaf97a432eb0ac7e
D'ORIO Giuseppe 2098 <giuseppe.dorio@csi.it>
2022-04-20 11:36:52 +0200

    fixed login

--- app/src/boot/keycloak.js
+++ app/src/boot/keycloak.js
@@ -62,9 +62,10 @@ export default async ({ Vue, router, store, app }) => {
         init: {
           onLoad: 'check-sso', // or 'check-sso'
           flow: 'standard',
-          pkceMethod: 'S256',
-          silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
-//          checkLoginIframe: false // otherwise it would reload the window every so seconds
+          //pkceMethod: 'S256',
+          //silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+          checkLoginIframe: false, // otherwise it would reload the window every so seconds,
+          //enableLogging:true
         },
         config: {
           url: 'https://identity.micadoproject.eu/auth',
